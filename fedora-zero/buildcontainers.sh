203d57bee68291cdbce19299ddb7659075b7933c
ENDOH takanao <djmchl@gmail.com>
2014-06-22 20:01:09 +0530

    use michilu/fedora-zero

--- fedora-zero/buildcontainers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-rm -rf /tmp/fedora*
-rm -rf *.tar.xz
-appliance-creator -c fedora-zero.ks -d -v -t /tmp \
-    -o /tmp --name "fedora-zero" --release rawhide \
-    --format=qcow2 &&
-virt-tar-out -a \
-    /tmp/fedora-zero.qcow2 / - | \
-    xz --best > fedora-zero.tar.xz
